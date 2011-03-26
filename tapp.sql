7061248a26a0af4a62de43a69eae2dcc02348331
Vayn <vayn@vayn.de>
2011-03-27 01:20:33 +0800

    update

--- tapp.sql
+++ tapp.sql
@@ -9,7 +9,7 @@ CREATE TABLE setting (
     id INTEGER NOT NULL PRIMARY KEY,
     uid INTEGER NOT NULL REFERENCES "users" ("id"),
     twitter VARCHAR(30) NOT NULL,
-    cron INTEGER NOT NULL DEFAULT 0,
+    cron INTEGER NOT NULL DEFAULT 1,
     cache_time INTEGER NOT NULL,
     update_time TIMESTAMP,
     amount INTEGER NOT NULL,
@@ -26,5 +26,5 @@ CREATE TABLE tweets (
 )
 ;
 INSERT INTO users VALUES(1, 'hal', '7f50914da1f9c70cf6359be4534e6ab0f7e1cbde');
-INSERT INTO setting VALUES(1, 1, 'twitter', 0, 1800, '', 1, 'json', 'no', '');
+INSERT INTO setting VALUES(1, 1, 'twitter', 1, 1800, '', 1, 'json', 'no', '');
 COMMIT;
